0ed5f27064e7cdb9d202e113bc4de0cf31eca234
Simel James <jameskarinosimel@gmail.com>
2022-04-24 14:05:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.getElementsByClassName("togglebtn")[0];
+
+const navLinks = document.getElementsByClassName("nav-bar")[0];
+
+toggleBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    }
+);
+
+$(".nav-bar").click(function(){
+    nav-bar.classList.toggle("active")
+});
